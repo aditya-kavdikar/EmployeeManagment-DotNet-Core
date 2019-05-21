560bfe056e4ece3cc29cc30b83e95b0b2ad360aa
Aditya Kavdikar <aditya.kavdikar@yash.com>
2019-05-21 13:06:28 +0530

    uploading project files

---
+++ EmployeeManagement/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace EmployeeManagement
+{
+    //usually console application has program class and Main() method 
+    //ASP .Net core app initially starts as console app and main() enrty point for
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //this method returns webHostBuilder that makes ASP.NET core app as web app
+            //Build() method host this app and Run() method starts app to listen to incoming http request
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        //CreateDefaultBuilder(args) static method that create web host with preconfigure defaults
+        //some of the tasks that CreateDefaultBuilder(args) performs
+        //1)setting up the web server
+        //2)Loading the host and application configuration from various configuration sources and
+        //3)configuring logging
+
+
+
+        //UseStartup(extension method) is configuring startup class
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
+    }
+}
---
+++ EmployeeManagement/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EmployeeManagement
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        //ByDefault VS uses iisexpress to host and run application
+        //iisexpress=it is a lightweight self contained version of iis optimised specially for developing app
+        //iisexpress is not used in production
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.
+                Response.WriteAsync("process name executing the app " + System.Diagnostics.Process.GetCurrentProcess().ProcessName);
+            });
+        }
+    }
+}
